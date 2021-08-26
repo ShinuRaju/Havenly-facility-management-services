2a8a3cc74975d0ab076da2e1099e2267a6021d9e
Shinu <rajushinu007@gmail.com>
2021-08-26 22:21:17 +0530

    added service, gallery and mission sections

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import { servicesList } from "./data.js";
+
+let serBox = document.querySelector(".servicesBox");
+let galleryBox = document.querySelector("#gallery");
+
 const addCss = (elem, style) => {
   elem = elem.trim();
   style = style.trim();
@@ -8,3 +13,29 @@ const addCss = (elem, style) => {
 
 addCss(".carousel-item h5", "display-6");
 addCss(".carousel-caption", "px-5");
+
+serBox.innerHTML = servicesList.map((eachList) => {
+  return `<h5> ${eachList.heading} </h5>
+            <ul>
+            <p>${eachList.p1 === undefined ? "" : eachList.p1}</p>
+            <p>${eachList.p2 === undefined ? "" : eachList.p2}</p>
+            <p>${eachList.p3 === undefined ? "" : eachList.p3}</p>
+            <p>${eachList.p4 === undefined ? "" : eachList.p4}</p>
+            <p>${eachList.p5 === undefined ? "" : eachList.p5}</p>
+            <p>${eachList.p6 === undefined ? "" : eachList.p6}</p>
+            
+            </ul>
+  `;
+});
+
+let wholeImages = [];
+
+for (let i = 1; i <= 36; i++) {
+  wholeImages.push(
+    `<img src="./images/gallery/galls${i
+      .toString()
+      .padStart(2, "0")}.jpg" alt="image${1}">`
+  );
+}
+
+galleryBox.innerHTML = wholeImages;
---
+++ data.js
@@ -0,0 +1,48 @@
+export const servicesList = [
+  {
+    heading: "SOFT SERVICES:",
+    p1: "House Keeping & Janitorial",
+    p2: "Pest Control",
+    p3: "Garden & Lawn Maintenance",
+    p4: "Waste Management",
+    p5: "Facade & Glass Cleaning",
+    p6: "Deep Cleaning of Factory Sheds",
+  },
+  {
+    heading: "HARD SERVICES:",
+    p1: "Electrical & Mechanical Maintenance",
+    p2: "HVAC",
+    p3: "Water Management & Plumbing",
+    p4: "STP / ETP Operations",
+    p5: "BMS Operations",
+    p6: "3rd Party / Vendor Management",
+  },
+  {
+    heading: "SUPPORT SERVICES:",
+    p1: "Staffing – Skilled & Semi skilled",
+    p2: "Payroll Management",
+    p3: "Mail Room Management",
+    p4: "Admin Outsourcing",
+  },
+  {
+    heading: "PRODUCTION SUPPORT / LOGISTICS:",
+    p1: "Line operations",
+    p2: "Assembly Activities",
+    p3: "Material Handling",
+    p4: "Date Entry Operators",
+    p5: "Welding & Painting",
+    p6: "Packaging Loading & Unloading",
+  },
+  {
+    heading: "HOSPITALITY SERVICES:",
+    p1: "Guest House Management",
+    p2: "Food Court Services",
+    p3: "Beverage Vending",
+    p4: "Pantry & Cafeteria Services",
+  },
+  {
+    heading: "OTHERS:",
+    p1: "Project works",
+    p2: "Interiors",
+  },
+];
